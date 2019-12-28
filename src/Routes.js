74fe5ba74f9ab19e429fd03786180e6e08eb07d0
Seung Hyun Soun <jamessoun93@gmail.com>
2019-12-29 02:38:27 +0900

    Initial setup for Model 3 page

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Model3 from "./pages/Model3";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/model3" component={Model3} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-export default Routes
+export default Routes;
---
+++ src/pages/Model3/data.js
@@ -0,0 +1,3 @@
+export const TOOLTIPS = [];
+
+export const ANCHORS = [];
