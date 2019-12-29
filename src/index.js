3c5baa3502bd81efae6fb6d72cbc4810c9ac238b
Seung Hyun Soun <jamessoun93@gmail.com>
2019-12-29 14:16:50 +0900

    lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 // import App from './App';
-import Routes from './Routes'
+import Routes from "./Routes";
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById("root"));
