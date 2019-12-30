c3a5fad108e6e856e7f32c9e07db4075758c92a7
Seung Hyun Soun <jamessoun93@gmail.com>
2019-12-30 22:15:47 +0900

    Initial setup for Model X page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ModelS from "./pages/ModelS";
 import Model3 from "./pages/Model3";
+import ModelX from "./pages/ModelX";
 import Shop from "./pages/Shop";
 import ShopSecond from "./pages/Shop/ShopSecond";
 import ShopThird from "./pages/Shop/ShopThird";
@@ -16,6 +17,7 @@ const Routes = () => {
         <Route exact path="/" component={Home} />
         <Route path="/models" component={ModelS} />
         <Route path="/model3" component={Model3} />
+        <Route path="/modelx" component={ModelX} />
         <Route path="/shop" component={Shop} />
         <Route path="/shop2" component={ShopSecond} />
         <Route path="/shop3" component={ShopThird} />
